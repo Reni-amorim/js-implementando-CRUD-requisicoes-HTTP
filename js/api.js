2803198b9fc7ad5b8c57d05415f1634de12b1b08
Reni-amorim <reni.425@gmail.com>
2025-04-04 17:58:33 -0300

    Refactor HTTP requests to use Axios
    - Add Axios dependency via CDN
    - Replace existing GET requests with Axios implementation
    - Inspect requests and handle status codes
    - Improve request error handling

--- js/api.js
+++ js/api.js
@@ -1,14 +1,61 @@
+const URL_BASE = "http://localhost:3000"
+
 const api = {
     async buscarPensamentos() {
         try {
-            const response = await fetch('http://localhost:3000/pensamentos')
-            return await response.json()
+            const response = await axios.get(`${URL_BASE}/pensamentos`)
+            return await response.data
         }
         catch {
             alert("Erro ao buscar pensamentos")
             throw error
         }
+    },
+
+    async salvarPensamento(pensamento) {
+        try {
+            const response = await axios.post(`${URL_BASE}/pensamentos`, pensamento)
+            return await response.data
+        }
+        catch {
+            alert("Erro ao salvar pensamentos")
+            throw error
+        }
+    },
+
+
+    async buscarPensamentoId(id) {
+        try {
+            const response = await axios.get(`${URL_BASE}/pensamentos/${id}`)
+            return await response.data
+        }
+        catch {
+            alert("Erro ao buscar pensamento")
+            throw error
+        }
+    },
+
+    async editarPensamento(pensamento) {
+        try {
+            const response = await axios.put(`${URL_BASE}/pensamentos/${pensamento.id}`, pensamento)
+            return await response.data
+        }
+        catch {
+            alert("Erro ao editar pensamentos")
+            throw error
+        }
+    },
+
+    async deletarPensamento(id) {
+        try {
+            const response = await axios.delete(`${URL_BASE}/pensamentos/${id}`)
+        }
+        catch {
+            alert("Erro ao excluir pensamentos")
+            throw error
+        }
     }
+
 }
 
 export default api;
